a32ceb399191f272e8c24ba4b9849bf4ee6ff5b7
MIZUKICHI-Lab <makebetter1147@gmail.com>
2024-10-10 21:03:29 +0900

    providerでrouterを管理するように修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tokyo_hakkason2024_app/router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
     return MaterialApp.router(
-      routerDelegate: goRouter.routerDelegate,
-      routeInformationParser: goRouter.routeInformationParser,
-      routeInformationProvider: goRouter.routeInformationProvider,
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,49 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tokyo_hakkason2024_app/features/auth/pages/login_page.dart';
 import 'package:tokyo_hakkason2024_app/features/auth/pages/signup_page.dart';
 import 'package:tokyo_hakkason2024_app/features/home/pages/home_page.dart';
 
-final goRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: 'initial',
-      pageBuilder: (context, state) {
-        return MaterialPage(
-          key: state.pageKey,
-          child: const HomePage(),
-        );
-      },
-    ),
-    GoRoute(
-      path: LoginPage.routeName,
-      name: LoginPage.routeName,
-      pageBuilder: (context, state) {
-        return MaterialPage(
-          key: state.pageKey,
-          child: const LoginPage(),
-        );
-      },
-    ),
-    GoRoute(
-      path: SigninPage.routeName,
-      name: SigninPage.routeName,
-      pageBuilder: (context, state) {
-        return MaterialPage(
-          key: state.pageKey,
-          child: const SigninPage(),
-        );
-      },
-    ),
-  ],
-  errorPageBuilder: (context, state) => MaterialPage(
-    key: state.pageKey,
-    child: Scaffold(
-      body: Center(
-        child: Text(state.error.toString()),
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        name: 'initial',
+        pageBuilder: (context, state) {
+          return MaterialPage(
+            key: state.pageKey,
+            child: const HomePage(),
+          );
+        },
+      ),
+      GoRoute(
+        path: LoginPage.routeName,
+        name: LoginPage.routeName,
+        pageBuilder: (context, state) {
+          return MaterialPage(
+            key: state.pageKey,
+            child: const LoginPage(),
+          );
+        },
+      ),
+      GoRoute(
+        path: SigninPage.routeName,
+        name: SigninPage.routeName,
+        pageBuilder: (context, state) {
+          return MaterialPage(
+            key: state.pageKey,
+            child: const SigninPage(),
+          );
+        },
+      ),
+    ],
+    errorPageBuilder: (context, state) => MaterialPage(
+      key: state.pageKey,
+      child: Scaffold(
+        body: Center(
+          child: Text(state.error.toString()),
+        ),
       ),
     ),
-  ),
-);
+  );
+});
