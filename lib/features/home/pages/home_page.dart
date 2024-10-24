cd02a4300bf3c1e14b83486feee82a0ac055d848
MIZUKICHI-Lab <makebetter1147@gmail.com>
2024-10-24 21:18:27 +0900

    supabaseとの繋ぎ込み、メール認証追加

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  static const String routeName = '/home';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Flutter Demo Home Page'),
-        ),      
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter'),
+      ),
     );
   }
 }
